dca5b3e41e6fade980c161e36bc40cf0b2ca6c6c
krunal B Thakkar <62111615+krunal43@users.noreply.github.com>
2021-09-10 16:27:36 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Jenkins integration with git successfully done");  
     }  
 }  
